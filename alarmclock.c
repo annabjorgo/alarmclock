a6b727848bda08916d6e3e9715a36de96d8193b3
annajbj <annajbj@stud.ntnu.no>
2022-02-16 17:05:15 +0100

    fixed error

--- alarmclock.c
+++ alarmclock.c
@@ -22,13 +22,20 @@ struct alarm_t alarmArray[20];
 void alarm_ring()
 {
     printf("\nRing ring bitch");
+    //Sound wont work because of WSL
     // execlp("mpg123", "mpg123", "-q", "./alarm.mp3", NULL);
 }
 
+unsigned int alarm_diff_time(time_t timestamp) {
+    time_t now = time(NULL);
+    int diff = (int)difftime(timestamp, now);
+    return diff;
+}
 
 unsigned int fork_alarm(time_t timestamp)
 {
     unsigned int pid = fork(); //should be a pid_t type 
+    int diff_time = alarm_diff_time(timestamp);
 
     if (pid != 0)
     {
@@ -37,54 +44,47 @@ unsigned int fork_alarm(time_t timestamp)
 
     else
     {
-        time_t now = time(NULL);
+        printf("Scheduling alarm in %d seconds\n", diff_time);
 
-
-        int diff = (int)difftime(timestamp, now);
-        printf("Scheduling alarm in %d seconds\n", diff);
-
-        if (diff < 0)
+        if (diff_time < 0)
         {
             // error handling
             exit(1);
         }
         else
         {
-            // printf("%d", diff);
-            sleep(diff);
+            sleep(diff_time);
             alarm_ring();
             exit(0);
         }
     }
 }
 
-void welcome() {
+void welcome_message() {
     time_t now = time(NULL);
     struct tm *time = localtime(&now);
     char s[100];
     strftime(s, 100, "%Y-%m-%d %X", time);
-    printf("Welcome to the alarm clock! It is currently %s", s);
+    printf("Welcome to the alarm clock! It is currently %s\n", s);
     printf("Please enter 's' (schedule), 'l' (list), 'c' (cancel) or 'x' (exit) \n");
-
 }
 
 void menuFunc()
 {
-    welcome();
     scanf("%[^\n]%*c", &menu_select);
     char alarmInput[LEN] = {0};
     if (menu_select == 's')
     {
         char buf[255];
-        printf("Schedule alarm at which date and time? ");
+        printf("Schedule alarm at which date and time? Format:YYYY-MM-DD hh:mm:ss ");
         //legge til hvis bruker skriver inn feil input
         scanf("%[^\n]%*c", alarmInput);
         struct tm result;
+        result.tm_isdst =-1;
         strptime(alarmInput, "%Y-%m-%d %H:%M:%S", &result);
 
         // convert from tm struct to time_t variable
         time_t resultTime = mktime(&result);
-        // print ut tid
 
         // call function that forks and creates a child process for the alarm
         unsigned int pid = fork_alarm(resultTime);
@@ -121,6 +121,7 @@ void menuFunc()
 int main()
 {
     alarm_t alarm_object;
+    welcome_message();
     while (number == 0)
     {
         menuFunc();
